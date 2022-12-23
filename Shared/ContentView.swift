52682c74e1cea6ad929d12c083e113ef96228bd0
RealWarrior03 <henry.krieger@campus.tu-berlin.de>
2022-12-23 23:11:14 +0100

    fixed build files

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,11 +10,6 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
     
     @StateObject var viewRouter: ViewRouter
 
